7dd395ee88fdb18ef8b55fc74d61d33000986487
UCB10116 <UCB10116@UCB10116>
2026-05-02 11:18:48 -0500

    inicializando

---
+++ src/instituto_innova/Instituto_innova.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package instituto_innova;
+
+/**
+ *
+ * @author UCB10116
+ */
+public class Instituto_innova {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        EstudianteControlador control = new EstudianteControlador();
+        String rpta = "s";
+
+        
+    }
+    
+}
